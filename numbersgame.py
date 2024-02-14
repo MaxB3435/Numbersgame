0f3aa9525f916a961eb95814f8e913c89067c51d
MaxB3435 <bortasmax@gmail.com>
2024-02-14 15:00:00 +0100

    Update numbersgame.py

--- numbersgame.py
+++ numbersgame.py
@@ -1,5 +1,20 @@
 import random
+ng = 1
+g = 0
 
-for a in range (100):
-    number = random.randint(1,100)
-    print (number)
+number = random.randint(1,100)
+print (number)
+while True:
+    g = int(input("Jag datorn tänker på ett heltal mellan 1 och 100. Vad gissar du?"))
+    if g != number:
+        ng = ng +1
+        
+        print(g)
+        if g > number:
+            print('Nix, lägre')
+        else:
+            print('Nix, högre')
+    else:
+        print('RÄTT! Du klarade spelet på', ng,'gissningar')
+        exit()
+    
